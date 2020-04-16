21e98b9a2c071c658e9d500123386b77b32d9621
Hanif437 <putrahanif437@gmail.com>
2020-04-16 14:22:14 +0700

    Create latihan1.php

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <table>
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th></th>
+    </tr>
+  </table>
+
+</body>
+
+</html>
