3c2d6ac843b03151a1c64d265bacbcca482aaa11
Hanif437 <putrahanif437@gmail.com>
2020-04-22 19:18:01 +0700

    Update functions.php

--- kuliah/pertemuan10/functions.php
+++ kuliah/pertemuan10/functions.php
@@ -1,19 +1,25 @@
 <?php
 //functions untuk melakukan koneksi ke database
-function koneksi(){
-    $conn = mysqli_connect("localhost", "root", "") or die ("Koneksi ke DB gagal!");
-    mysqli_select_db($conn, "pw_193040001") or die ("Database Salah!");
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "") or die("Koneksi ke DB gagal!");
+    mysqli_select_db($conn, "pw_193040001") or die("Database Salah!");
 
     return $conn;
 }
 
 //function untuk melakukan query ke database
-function query ($sql){
+function query($sql)
+{
     $conn = koneksi();
     $result = mysqli_query($conn, "$sql");
 
+    if (mysqli_num_rows($result) == 1) {
+        return mysqli_fetch_assoc($result);
+    }
+
     $rows = [];
-    while ($row = mysqli_fetch_assoc($result)){
+    while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
     return $rows;
