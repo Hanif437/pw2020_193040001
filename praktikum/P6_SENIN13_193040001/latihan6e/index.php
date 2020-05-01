6c8b78ebf285d1b25ba4430aef5dd7375afa6ec0
Hanif437 <putrahanif437@gmail.com>
2020-05-01 23:44:00 +0700

    Create index.php

---
+++ praktikum/P6_SENIN13_193040001/latihan6e/index.php
@@ -0,0 +1,79 @@
+<?php
+require 'php/functions.php';
+$elektronik = query("SELECT * FROM elektronik");
+// var_dump($elektronik);
+// foreach($elektronik as $elektro){
+// echo $elektro['nama'];
+// }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 6 E</title>
+    <style>
+        .container {
+            border: 5px solid seagreen;
+            width: 580px;
+            height: 320px;
+            padding-top: 10px;
+            padding-left: 10px;
+            border-radius: 30px;
+            margin: auto;
+            font-size: x-large;
+            font-family: arial;
+            background-color: black;
+        }
+
+        a:active {
+            text-decoration: none;
+            color: magenta;
+        }
+
+        a:link {
+            text-decoration: none;
+            color: salmon;
+        }
+
+        a:hover {
+            text-decoration: none;
+            color: seagreen;
+        }
+
+        .admin {
+            background-color: black;
+            color: white;
+            border-radius: 10px;
+            margin: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>
+        <center>DAFTAR BARANG ELEKTRONIK</center>
+    </h1>
+    <div class="container">
+        <?php foreach ($elektronik as $elektro) : ?>
+            <div class="nama">
+                <a href="php/detail.php?id=<?= $elektro['id']; ?>">
+                    <?= $elektro["nama"]; ?>
+                </a>
+            </div>
+        <?php endforeach; ?>
+        <center>
+            <button class="admin">
+                <a href="php/admin.php">Admin Disini!!!</a>
+            </button>
+        </center>
+    </div>
+    <h1>
+        <center>ANDA PUAS, KAMI SENANG!!!</center>
+    </h1>
+</body>
+
+</html>
