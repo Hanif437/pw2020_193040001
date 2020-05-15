885da25c0a6cae37d54f3e3738e5fe11df6140f8
Hanif437 <putrahanif437@gmail.com>
2020-05-15 19:33:12 +0700

    Create index.php
    some errors and fixed soon

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Website</title>
+  <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body>
+  <!-- Header -->
+  <header id="header">
+    <div class="inner">
+      <a href="index.php" class="logo"><strong>Putra Hanif Ammarta</strong></a>
+      <nav id="nav">
+        <a href="index.php">Home</a>
+      </nav>
+      <a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+    </div>
+  </header>
+
+  <!-- Banner -->
+
+  <section id="banner">
+    <div class="inner">
+      <header>
+        <h1>Selamat Datang di Tugas Besar Hanif</h1>
+      </header>
+      <div class="flex ">
+
+        <div>
+          <span class="icon fa-phone"></span>
+          <h3>Handphone</h3>
+          <p>Barang untuk berkomunikasi dan mencari seputar informasi terkini</p>
+        </div>
+
+        <div>
+          <span class="icon fa-camera"></span>
+          <h3>Kamera</h3>
+          <p>Barang untuk mengambil sebuah gambar dari objek</p>
+        </div>
+
+        <!-- <div>
+          <span class="icon fa-car"></span>
+          <h3>Mobil</h3>
+          <p>Alat transportasi beroda empat di jalur darat</p>
+        </div> -->
+
+      </div>
+
+      <footer>
+        <a href="#three" class="button">Ayo Mulai</a>
+      </footer>
+    </div>
+  </section>
+
+
+  <!-- Three -->
+  <section id="three" class="wrapper align-center">
+    <div class="inner">
+      <div class="flex flex-4">
+        <article>
+          <header>
+            <h3>P2_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P2_SENIN13_193040001/latihan2a.php" class="button special">Latihan 2 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P2_SENIN13_193040001/latihan2b.php" class="button special">Latihan 2 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P2_SENIN13_193040001/latihan2c.php" class="button special">Latihan 2 C</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P2_SENIN13_193040001/tugas1.php" class="button special">Tugas 2</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>P3_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P3_SENIN13_193040001/latihan3a.php" class="button special">Latihan 3 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P3_SENIN13_193040001/latihan3b.php" class="button special">Latihan 3 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P3_SENIN13_193040001/latihan3c.php" class="button special">Latihan 3 C</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P3_SENIN13_193040001/latihan3d.php" class="button special">Latihan 3 D</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>P4_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P4_SENIN13_193040001/latihan4a.php" class="button special">Latihan 4 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P4_SENIN13_193040001/latihan4b.php" class="button special">Latihan 4 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P4_SENIN13_193040001/latihan4c.php" class="button special">Latihan 4 C</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P4_SENIN13_193040001/latihan4d.php" class="button special">Latihan 4 D</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P4_SENIN13_193040001/tugas4.php" class="button special">Tugas 4</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>P5_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P5_SENIN13_193040001/latihan5a/index.php" class="button special">Latihan 5 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P5_SENIN13_193040001/latihan5b/index.php" class="button special">Latihan 5 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P5_SENIN13_193040001/latihan5c/index.php" class="button special">Latihan 5 C</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>P6_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/latihan6a/index.php" class="button special">Latihan 6 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/latihan6b/index.php" class="button special">Latihan 6 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/latihan6c/index.php" class="button special">Latihan 6 C</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/latihan6d/index.php" class="button special">Latihan 6 D</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/latihan6e/index.php" class="button special">Latihan 6 E</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P6_SENIN13_193040001/tugas6/index.php" class="button special">Tugas 6</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>P7_SENIN13_193040001</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada praktikum</p>
+          <footer>
+            <a href="praktikum/P7_SENIN13_193040001/latihan7a/index.php" class="button special">Latihan 7 A</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P7_SENIN13_193040001/latihan7b/index.php" class="button special">Latihan 7 B</a>
+          </footer>
+          <footer>
+            <a href="praktikum/P7_SENIN13_193040001/latihan7c/index.php" class="button special">Latihan 7 C</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>Tugas Besar</h3>
+          </header>
+          <p>Hasil tugas besar</p>
+          <footer>
+            <a href="tubes/index.php" class="button special">Tugas Besar</a>
+          </footer>
+        </article>
+        <article>
+          <header>
+            <h3>Kuliah</h3>
+          </header>
+          <p>Kumpulan tugas dan latihan pada perkuliahan</p>
+          <footer>
+            <a href="kuliah/pertemuan10/latihan1.php" class="button">Pertemuan ke 10</a>
+          </footer>
+          <footer>
+            <a href="kuliah/pertemuan11/index.php" class="button">Pertemuan ke 11</a>
+          </footer>
+          <footer>
+            <a href="kuliah/pertemuan12/index.php" class="button">Pertemuan ke 12</a>
+          </footer>
+        </article>
+      </div>
+    </div>
+  </section>
+
+
+
+  <!-- Footer -->
+  <!-- <footer id="footer">
+    <div class="inner">
+
+      <h3>Get in touch</h3>
+
+      <form action="#" method="post">
+
+        <div class="field half first">
+          <label for="name">Name</label>
+          <input name="name" id="name" type="text" placeholder="Name">
+        </div>
+        <div class="field half">
+          <label for="email">Email</label>
+          <input name="email" id="email" type="email" placeholder="Email">
+        </div>
+        <div class="field">
+          <label for="message">Message</label>
+          <textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+        </div>
+        <ul class="actions">
+          <li><input value="Send Message" class="button alt" type="submit"></li>
+        </ul>
+      </form> -->
+
+  <div class="copyright">
+    &copy; Putra Hanif Ammarta.2020</a>.
+  </div>
+
+  </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/skel.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
